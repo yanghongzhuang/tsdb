a956f217a274711b237945c9b063fcf5cf6dbd6c
zhuang <zhuang@zhuangdeMacBook-Pro.local>
2020-04-04 22:28:21 +0800

    base data structure

--- src/main/java/zhuang/tsdb/Metrics.java
+++ src/main/java/zhuang/tsdb/Metrics.java
@@ -1,4 +1,42 @@
 package zhuang.tsdb;
 
+import java.util.Map;
+
 public class Metrics {
+    private long time;
+    private String key;
+    private double value;
+    private Map<String, String> tags;
+
+    public long getTime() {
+        return time;
+    }
+
+    public void setTime(long time) {
+        this.time = time;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+
+    public Map<String, String> getTags() {
+        return tags;
+    }
+
+    public void setTags(Map<String, String> tags) {
+        this.tags = tags;
+    }
 }
